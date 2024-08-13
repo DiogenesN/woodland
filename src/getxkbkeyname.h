1c7609e819f409c326fdab5d3fb7a548717b4ecf
Nicolas <147919918+DiogenesN@users.noreply.github.com>
2024-08-13 09:29:27 +0300

    Delete src directory

--- src/getxkbkeyname.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#ifndef GETXKBKEYNAME_H_
-#define GETXKBKEYNAME_H_
-char *xkb_keyname(const char *hexadecimal);
-#endif
--- src/runcmd.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#ifndef RUNCMD_H_
-#define RUNCMD_H_
-	void run_cmd(char *command);
-#endif
